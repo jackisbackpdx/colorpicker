3f58a1195351f6bbe69433dfa12950b6a1056982
Jackson Kelley <jackson.kelley.g@gmail.com>
2020-01-28 14:22:09 -0800

    moved color picker app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SquaresWithValues extends React.Component {
+  render() {
+    return (
+        <button 
+        className={this.props.value}
+        onClick={this.props.onClick}>
+        {this.props.value}
+        </button>
+      );
+  }      
 }
 
-export default App;
+class ChangedColor extends React.Component {
+  render() {
+    return (
+      <div className={this.props.value + '-changed'}>
+
+      </div>
+    )
+  }
+}
+
+class MakeSquares extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { colors: [ 'blue', 'yellow', 'red', 'red' ] }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(e) {
+    e.preventDefault();
+          let color = this.state.colors.slice();
+          color[3] = e.target.className;
+          this.setState({colors: color})
+  }
+
+  renderColor(i) {
+    return <ChangedColor value={this.state.colors[i]}/>
+  }
+  
+  renderButton(i) {
+    return <SquaresWithValues value={this.state.colors[i]}
+            onClick={(e) => this.handleClick(e)}/>;          
+  }
+
+  render() {
+    return (
+      <div id='outside'>
+      <h1>REACT COLOR PICKER</h1>
+      <div className='choices'>
+        {this.renderButton(0)}
+        {this.renderButton(1)}
+        {this.renderButton(2)}
+      </div>
+      <div id='changed'>
+        {this.renderColor(3)}
+      </div>
+      </div>
+    );
+  }
+}
+
+export default MakeSquares;
